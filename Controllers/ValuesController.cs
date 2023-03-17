8614ed931a446f3de54b646bc2c03e2eec655c99
TrickSoLogic <vishalbhat.com@gmail.com>
2023-03-17 13:30:13 +0530

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using BlackCoderWeatherAPICALL.Data;
+using BlackCoderWeatherAPICALL.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackCoderWeatherAPICALL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        ConnectionDbContext db;
+        public ValuesController(ConnectionDbContext _db)
+        {
+                db = _db;
+        }
+        [HttpGet]
+        public ActionResult GetData()
+        {
+            return Ok(db.saleData.ToList());
+        }
+        
+    }
+}
---
+++ Data/ConnectionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlackCoderWeatherAPICALL.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlackCoderWeatherAPICALL.Data
+{
+    public class ConnectionDbContext:DbContext
+    {
+        public ConnectionDbContext(DbContextOptions options):base(options)
+        {
+
+
+        }
+        public DbSet<SaleData> saleData { get; set; }
+    }
+}
